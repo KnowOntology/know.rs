f593969a5742b55808940da9bc2249d901e2bd66
Arto Bendiken <arto@haltia.ai>
2023-11-10 01:11:28 +0400

    Create the project.

---
+++ lib/know/src/feature.rs
@@ -0,0 +1,7 @@
+// This is free and unencumbered software released into the public domain.
+
+#[allow(unused)]
+pub static FEATURES: &[&str] = &[
+    #[cfg(feature = "serde")]
+    "serde",
+];
---
+++ lib/know/src/lib.rs
@@ -0,0 +1,7 @@
+// This is free and unencumbered software released into the public domain.
+
+mod error;
+mod feature;
+
+pub use error::*;
+pub use feature::*;
